ca042da5a1740ce412859c84a52925295892320e
Sam <sandeshmas@gmail.com>
2022-07-17 22:11:29 -0700

    Made changes on how cards are created

--- scripts.js
+++ scripts.js
@@ -1,19 +1,56 @@
 //Listen to click on Add to List button
 const addToList = document.getElementById("add");
-addToList.addEventListener("click", (obj) => {
-  obj.preventDefault();
-  const destinationName = document.getElementById("destination_name").value;
-  const location = document.getElementById("location_name").value;
-  const photo = document.getElementById("photo_url").value;
-  const description = document.getElementById("details").value;
-  const card = `
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${photo}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">${destinationName}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${location}</h6> 
-            <p class="card-text">${description}.</p>
-        </div>
-    </div>`;
-  document.getElementById("bootstrap_card").innerHTML = card;
+addToList.addEventListener("click", (addClick) => {
+  addClick.preventDefault();
+  let destinationName = document.getElementById("destination_name").value;
+  let locationName = document.getElementById("location_name").value;
+  let photoURL = document.getElementById("photo_url").value;
+  let description = document.getElementById("details").value;
+
+  let newCard = createCard(
+    destinationName,
+    locationName,
+    photoURL,
+    description
+  );
+  document.getElementById("bootstrap_card").appendChild(newCard);
 });
+
+function createCard(destinationName, location, photoURL, description) {
+  //Div to cover the complete card
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  //Added the image to the card
+  let photo = document.createElement("img");
+  photo.setAttribute("class", "card-top-image");
+  photo.setAttribute("src", "photoURL");
+  photo.setAttribute("alt", "Picture of Destination");
+  card.appendChild(photo);
+
+  //Div to cover body of card
+  let cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
+
+  //Added the destination to the card
+  let dName = document.createElement("h5");
+  dName.setAttribute("class", "card-title");
+  dName.innerHTML = destinationName;
+  cardBody.appendChild(dName);
+
+  //Added the location to the card
+  let lName = document.createElement("h6");
+  lName.setAttribute("class", "card-subtitle mb-2 text-muted");
+  lName.innerHTML = location;
+  cardBody.appendChild(lName);
+
+  //Added the description to the card
+  let desc = document.createElement("p");
+  desc.setAttribute("class", "card-text");
+  desc.innerHTML = description;
+  cardBody.appendChild(desc);
+
+  card.appendChild(cardBody);
+
+  return card;
+}
