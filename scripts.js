8ea5c2f4688a81a2aeb9c5a74ba776a3839533ea
Sam <sandeshmas@gmail.com>
2022-07-17 22:32:05 -0700

    Added the edit and remove buttons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//Listen to click on Add to List button
+//Listen to click on AddtoList button
 const addToList = document.getElementById("add");
 addToList.addEventListener("click", (addClick) => {
   addClick.preventDefault();
@@ -7,6 +7,7 @@ addToList.addEventListener("click", (addClick) => {
   let photoURL = document.getElementById("photo_url").value;
   let description = document.getElementById("details").value;
 
+  //Calling a fucntion to create new card
   let newCard = createCard(
     destinationName,
     locationName,
@@ -16,6 +17,7 @@ addToList.addEventListener("click", (addClick) => {
   document.getElementById("bootstrap_card").appendChild(newCard);
 });
 
+//Function to create a card for every new information
 function createCard(destinationName, location, photoURL, description) {
   //Div to cover the complete card
   let card = document.createElement("div");
@@ -24,7 +26,7 @@ function createCard(destinationName, location, photoURL, description) {
   //Added the image to the card
   let photo = document.createElement("img");
   photo.setAttribute("class", "card-top-image");
-  photo.setAttribute("src", "photoURL");
+  photo.setAttribute("src", photoURL);
   photo.setAttribute("alt", "Picture of Destination");
   card.appendChild(photo);
 
@@ -51,6 +53,30 @@ function createCard(destinationName, location, photoURL, description) {
   cardBody.appendChild(desc);
 
   card.appendChild(cardBody);
+
+  //Div to cover Edit and Remove buttons
+  let cardBtns = document.createElement("div");
+  cardBtns.setAttribute("class", "card-buttons");
+
+  //Added Edit button
+  let editBtn = document.createElement("button");
+  editBtn.setAttribute("class", "btn btn-edit");
+  editBtn.innerHTML = "Edit";
+  editBtn.addEventListener("click", (editClick) => {
+    alert("Edit Clicked");
+  });
+  cardBtns.appendChild(editBtn);
+
+  //Added Remove button
+  let removeBtn = document.createElement("button");
+  removeBtn.setAttribute("class", "btn btn-remove");
+  removeBtn.innerHTML = "Remove";
+  removeBtn.addEventListener("click", (removeClick) => {
+    alert("Remove Clicked");
+  });
+  cardBtns.appendChild(removeBtn);
+
+  card.appendChild(cardBtns);
 
   return card;
 }
